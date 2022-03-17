a2425b35b7096ff573e8cb872080b25dea03d208
andriali044 <andriali044gmail.com>
2022-03-17 08:34:35 +0700

    new lifecycle

--- tasklifecycle22/src/App.js
+++ tasklifecycle22/src/App.js
@@ -3,51 +3,66 @@ import React, { Component } from "react";
 // import './App.css';
 class App extends React.Component {
   constructor(){
-    super();
+    super()
+    // inisiasi state awal
     this.state = {
-      articles: [],
-      search: ""
+      search: "*",
+      articles: []
+      
     }
   }
-
-  //taruh statenya di dalam q=...
+  
+  // di jalankan saat pertama kali komponen di render
   componentDidMount() {
-    fetch("https://newsapi.org/v2/everything?q=tesla&from=2022-02-13&sortBy=publishedAt&apiKey=4da4024fcb0b490cb56b031302a4a808")
+    this.getNews();
+  }
+
+  // fungsi ini untuk mengambil data API ke newsapi menggunakan fetch
+  getNews = async () => {
+    const url = `https://newsapi.org/v2/everything?q=${this.state.search}&from=2022-02-16&sortBy=publishedAt&apiKey=4da4024fcb0b490cb56b031302a4a808`
+    fetch(url)
     .then((response) => {
+      console.log({response})
       return response.json();
     })
     .then((myJson) => {
+      console.log({myJson})
+      // setState di gunakan untuk menyimpan data json dari API ke dalam state.sarticles
       this.setState({
         articles: myJson.articles
       });
     });
   }
-  
+
+// FUngsi ini untuk memperbaharui nilai state.search
 changeNews = (e) => {
   this.setState({ [e.target.name] : e.target.value })
 }
 
+// fungsi ini ke triger saat onSubmit
 searchNews = (e) => {
   e.preventDefault();
-  
-  
-  this.setState({ [e.target.name] : e.target.value})
+
+  // cara memmanggil fungsi getNews
+  this.getNews();
 }
-//buat function untuk onSubmit
 
   render() {
-    console.log(this.state);
     return (
       <div>
         <div>
-          <form onSubmit={this.searchNews}> {/* edit lagi */}
+          {/* onSubmit ke triger saat tombol submit di klik */}
+          <form onSubmit={this.searchNews}>
             <label className='label'>Searching News with NewsAPI</label>
             <input type="text" name='search' value={this.state.search} className='form-control my-1'
             onChange={this.changeNews} placeholder='input your search in here' />
-            <input type="submit" value="Submit" className='btn btn-primary' />
+            {/* onChange keTriger saat nilai input berubah */}
+            <input type="submit" value="andri mencari" className='btn btn-primary' />
           </form>
+          <div className="row"></div>
         </div>
           <div className="App">
+            {/* Mapping nilai dari state.articles */}
             {this.state.articles.map((item, index)=>{
               return (
                 <div key={index}>
